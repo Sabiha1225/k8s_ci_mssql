7daa942e18293a27225963ba884bb8177647ae86
Sabiha1225 <1015052012.sa@grad.cse.buet.ac.bd>
2021-10-09 00:59:59 +0600

    first commit

---
+++ src/application/views/user_view.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>  
+<html>  
+<head>  
+    <title></title>  
+</head>  
+<body>  
+    <h1>Welcome <?php if (isset($full_name)) echo $full_name; ?></h1>   
+    <h1>Database Insertion <?php if (isset($insert)) echo $insert; ?></h1> 
+  
+</body>  
+</html> 
